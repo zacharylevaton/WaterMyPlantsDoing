2c5d686dae19a0e73be14210620c9a393728053a
Zach Levaton <zach.levaton.0514@gmail.com>
2021-05-22 16:12:12 -0700

    Initial commit setting up Flask app folder structure.

---
+++ waterMyPlantsDoing/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template, url_for, redirect, request, flash
+from waterMyPlantsDoing import app
+from datetime import datetime
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
